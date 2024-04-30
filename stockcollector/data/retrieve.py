7771c5ffa1cdaaa78cc7d6193d6ad60484006d03
Volkan Eymir Akcora <volkan.eymir.akcora@deutsche-boerse.com>
2024-04-30 23:14:26 +0200

    F

--- stockcollector/data/retrieve.py
+++ stockcollector/data/retrieve.py
@@ -3,12 +3,14 @@ import datetime
 from .models import Stock
 from .utils.functions import get_day_of_the_month, get_stock_prices
 from .utils.micro_macro_functions import micro_functions
-import pandas as pd 
+import pandas as pd
 from django.conf import settings
-from django.apps import AppConfig 
+from django.apps import AppConfig
 from .apps import AppConfig
 from .models import Stock
 import logging
+
+
 class stocks(AppConfig):
     def __init__(self, app_name: str, app_module: None) -> None:
         super().__init__(app_name, app_module)
@@ -16,48 +18,50 @@ class stocks(AppConfig):
     def run(self):
         try:
             self.retrieve_data()
-        except Exception:
-            logging.warn("WARN: Process failed")
+        except:
+            logging.warn("Process failed")
 
-    def retrieve_data():
+    def retrieve_data(self):
         time = datetime.datetime.now()
-        print('Stock Data collection has started:[{}]'.format(time))
-        startDate = '2023-02-01'
+        print("Stock Data collection has started:[{}]".format(time))
+        startDate = "2023-02-01"
         endDate = get_day_of_the_month()
         company = []
         stock_data = pd.DataFrame()
 
-        for key, value in settings.COMPANIES['default'].items():
+        for key, value in settings.COMPANIES["default"].items():
             company.append(value)
         print(company)
 
         for companies in company:
-        #     # Fetch data using get_stock_prices
-              data = get_stock_prices(startDate, endDate, companies)
-              stock_data = pd.concat([stock_data, data])
-  
+            #     # Fetch data using get_stock_prices
+            data = get_stock_prices(startDate, endDate, companies)
+            stock_data = pd.concat([stock_data, data])
+
         # Process and calculate values
         stock_data.reset_index(drop=True, inplace=True)  # Update in-place
-        stock_data['count'] = stock_data.index
+        stock_data["count"] = stock_data.index
         micro_functions.calc_vol(stock_data)
 
         # Delete the table firs
         Stock.objects.all().delete()
         # Save data to database using Django ORM
-        Stock.objects.bulk_create([
-            Stock(
-                date=row['Date'],
-                open_price=row['Open'],  # Renamed
-                high_price=row['High'],  # Renamed
-                low_price=row['Low'],  # Renamed
-                close_price=row['Close'],  # Renamed
-                volume=row['Volume'],
-                stock_name=row['ticker'],  # Renamed
-                daily_return=row['returns'],  # Renamed
-                volatility=row['volatility'],
-                daily_change=row['change'],  # Renamed
-                high_low_spread=row['hi_low_spread'],
-                expected_change=row['exp_change'],
-            ) for _, row in stock_data.iterrows()
-        ])
-        
+        Stock.objects.bulk_create(
+            [
+                Stock(
+                    date=row["Date"],
+                    open_price=row["Open"],  # Renamed
+                    high_price=row["High"],  # Renamed
+                    low_price=row["Low"],  # Renamed
+                    close_price=row["Close"],  # Renamed
+                    volume=row["Volume"],
+                    stock_name=row["ticker"],  # Renamed
+                    daily_return=row["returns"],  # Renamed
+                    volatility=row["volatility"],
+                    daily_change=row["change"],  # Renamed
+                    high_low_spread=row["hi_low_spread"],
+                    expected_change=row["exp_change"],
+                )
+                for _, row in stock_data.iterrows()
+            ]
+        )
--- stockcollector/stockcollector/operator.py
+++ stockcollector/stockcollector/operator.py
@@ -9,17 +9,18 @@ def start():
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     register_events(scheduler)
 
-    @scheduler.scheduled_job('interval', hours=10, name='retrieve_data')
+    @scheduler.scheduled_job('interval', hours=24, name='retrieve_data')
     def retrieve_data():
         from django.apps import apps  # Import apps module
         stocks_app = apps.get_app_config('data')  # Get the app config
         stocks_obj = stocks(stocks_app.name, stocks_app.module)
         stocks_obj.run()
     
-    @scheduler.scheduled_job('interval', hours=13, name='send_email')
+    @scheduler.scheduled_job('interval', hours=27, name='send_email')
     def send_email():
         from django.apps import apps  # Import apps module
         email_app = apps.get_app_config('data')  # Get the app config
         email_obj = stock_analytics(email_app.name, email_app.module)
         email_obj.run()
+    retrieve_data()
     scheduler.start()
